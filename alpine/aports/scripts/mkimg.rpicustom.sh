11ba4657722a93015cb3701fd5bb7fabdcd04a75
foxt <me@foxt.dev>
2024-04-16 19:59:59 +0100

    re-enable armhf & armv7

--- alpine/aports/scripts/mkimg.rpicustom.sh
+++ alpine/aports/scripts/mkimg.rpicustom.sh
@@ -2,6 +2,5 @@ profile_rpicustom() {
     profile_rpi
     title="Raspberry Pi Custom"
     desc="Built by Raspberry Pi Alpine Image Builder"
-    arch="aarch64"   
     apkovl="genapkovl-rpicustom.sh"
 }
